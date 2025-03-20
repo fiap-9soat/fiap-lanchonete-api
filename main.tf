b8b9e0b50f5d8f6a7e91c8426f187dbfc82be3af
daftK <du_ikei@hotmail.com>
2025-03-20 17:28:43 -0300

    wip

--- main.tf
+++ main.tf
@@ -6,19 +6,18 @@ data "aws_eks_cluster_auth" "eks_cluster_auth" {
   name = var.eks_cluster_name
 }
 
-module "configmap" {
-  source = "0-configmap.tf"
-}
-
-module "secret" {
-  source     = "1-secret.tf"
-  depends_on = [module.configmap]
-}
-
 module "deployment" {
-  source     = "2-deployment.tf"
-  depends_on = [module.secret]
-  vars = {
-    db_url = var.db_url
-  }
+  source = "./deploy"
+
+  aws_region            = var.aws_region
+  aws_access_key        = var.aws_access_key
+  aws_secret_key        = var.aws_secret_key
+  aws_token_key         = var.aws_token_key
+  db_url                = var.db_url
+  db_username           = var.db_username
+  db_password           = var.db_password
+  mercado_pago_api_key  = var.mercado_pago_api_key
+  mercado_pago_id_loja  = var.mercado_pago_id_loja
+  mercado_pago_id_conta = var.mercado_pago_id_conta
+  eks_cluster_name      = var.eks_cluster_name
 }
