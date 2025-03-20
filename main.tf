a6fa640f86e4f69acf2e011234d508c0ef29fde7
daftK <du_ikei@hotmail.com>
2025-03-20 16:59:31 -0300

    wip

--- main.tf
+++ main.tf
@@ -5,3 +5,20 @@ data "aws_eks_cluster" "eks_cluster" {
 data "aws_eks_cluster_auth" "eks_cluster_auth" {
   name = var.eks_cluster_name
 }
+
+module "configmap" {
+  source = "0-configmap.tf"
+}
+
+module "secret" {
+  source     = "1-secret.tf"
+  depends_on = [module.configmap]
+}
+
+module "deployment" {
+  source     = "2-deployment.tf"
+  depends_on = [module.secret]
+  vars = {
+    db_url = var.db_url
+  }
+}
